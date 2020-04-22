360d52637006b926dbf4922e0068843240d02802
Alex Maslovets <alexofun2009@yandex.ru>
2020-04-22 09:22:17 +0700

    DL-22-04-20

---
+++ NewDL/NewDL/Forms/mainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewDL
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private List<OdbcSource> sources;
+
+        private void Load_Form(object sender, EventArgs e)
+        {
+            odbcServers = DataAcess.ListODBCsources();
+            Reffesh_ComboBox();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            odbcServers = DataAcess.ListODBCsources();
+            Reffesh_ComboBox();
+        }
+
+        private void Reffesh_ComboBox()
+        {
+            comboBox1.Items.Clear();
+            foreach (string element in odbcServers)
+            {
+                comboBox1.Items.Add(element);
+            }
+        }
+    }
+}
