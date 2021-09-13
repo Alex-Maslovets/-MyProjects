a51f1c233ff012783eb0a6924e79eabfe9a7d537
Alex-Maslovets <61568256+Alex-Maslovets@users.noreply.github.com>
2021-09-13 22:06:27 +0700

    working dataCollection

--- Heineken_DataCollection/Heineken_DataCollection/Form1.Designer.cs
+++ Heineken_DataCollection/Heineken_DataCollection/Form1.Designer.cs
@@ -29,38 +29,15 @@ namespace Heineken_DataCollection
         /// </summary>
         private void InitializeComponent()
         {
-            this.Button_Connect = new System.Windows.Forms.Button();
-            this.Button_disConnect = new System.Windows.Forms.Button();
             this.Button_Read = new System.Windows.Forms.Button();
             this.Button_notRead = new System.Windows.Forms.Button();
-            this.progressBarConncet = new System.Windows.Forms.ProgressBar();
             this.progressBarRead = new System.Windows.Forms.ProgressBar();
             this.backgroundWorkerRead = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
-            // Button_Connect
-            // 
-            this.Button_Connect.Location = new System.Drawing.Point(76, 12);
-            this.Button_Connect.Name = "Button_Connect";
-            this.Button_Connect.Size = new System.Drawing.Size(176, 23);
-            this.Button_Connect.TabIndex = 0;
-            this.Button_Connect.Text = "Подключиться";
-            this.Button_Connect.UseVisualStyleBackColor = true;
-            this.Button_Connect.Click += new System.EventHandler(this.Button_Connect_Click);
-            // 
-            // Button_disConnect
-            // 
-            this.Button_disConnect.Location = new System.Drawing.Point(76, 41);
-            this.Button_disConnect.Name = "Button_disConnect";
-            this.Button_disConnect.Size = new System.Drawing.Size(176, 23);
-            this.Button_disConnect.TabIndex = 1;
-            this.Button_disConnect.Text = "Отключиться";
-            this.Button_disConnect.UseVisualStyleBackColor = true;
-            this.Button_disConnect.Click += new System.EventHandler(this.Button_disConnect_Click);
-            // 
             // Button_Read
             // 
-            this.Button_Read.Location = new System.Drawing.Point(76, 99);
+            this.Button_Read.Location = new System.Drawing.Point(12, 12);
             this.Button_Read.Name = "Button_Read";
             this.Button_Read.Size = new System.Drawing.Size(176, 23);
             this.Button_Read.TabIndex = 2;
@@ -70,7 +47,7 @@ namespace Heineken_DataCollection
             // 
             // Button_notRead
             // 
-            this.Button_notRead.Location = new System.Drawing.Point(76, 128);
+            this.Button_notRead.Location = new System.Drawing.Point(12, 41);
             this.Button_notRead.Name = "Button_notRead";
             this.Button_notRead.Size = new System.Drawing.Size(176, 23);
             this.Button_notRead.TabIndex = 3;
@@ -78,44 +55,32 @@ namespace Heineken_DataCollection
             this.Button_notRead.UseVisualStyleBackColor = true;
             this.Button_notRead.Click += new System.EventHandler(this.Button_notRead_Click);
             // 
-            // progressBarConncet
-            // 
-            this.progressBarConncet.Location = new System.Drawing.Point(76, 70);
-            this.progressBarConncet.Name = "progressBarConncet";
-            this.progressBarConncet.Size = new System.Drawing.Size(176, 23);
-            this.progressBarConncet.TabIndex = 4;
-            // 
             // progressBarRead
             // 
-            this.progressBarRead.Location = new System.Drawing.Point(76, 157);
+            this.progressBarRead.Location = new System.Drawing.Point(12, 70);
             this.progressBarRead.Name = "progressBarRead";
             this.progressBarRead.Size = new System.Drawing.Size(176, 23);
+            this.progressBarRead.Step = 1;
             this.progressBarRead.TabIndex = 5;
+            this.progressBarRead.MarqueeAnimationSpeed = 50;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 213);
+            this.ClientSize = new System.Drawing.Size(198, 106);
             this.Controls.Add(this.progressBarRead);
-            this.Controls.Add(this.progressBarConncet);
             this.Controls.Add(this.Button_notRead);
             this.Controls.Add(this.Button_Read);
-            this.Controls.Add(this.Button_disConnect);
-            this.Controls.Add(this.Button_Connect);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "HeinekenDC";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Button_Connect;
-        private System.Windows.Forms.Button Button_disConnect;
         private System.Windows.Forms.Button Button_Read;
         private System.Windows.Forms.Button Button_notRead;
-        private System.Windows.Forms.ProgressBar progressBarConncet;
         private System.Windows.Forms.ProgressBar progressBarRead;
         private System.ComponentModel.BackgroundWorker backgroundWorkerRead;
     }
--- Heineken_DataCollection/Heineken_DataCollection/Form1.cs
+++ Heineken_DataCollection/Heineken_DataCollection/Form1.cs
@@ -1,15 +1,10 @@
-﻿using System;
+﻿using Npgsql;
+using Sharp7;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Sharp7;
-using Npgsql;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Heineken_DataCollection
 {
@@ -24,61 +19,13 @@ namespace Heineken_DataCollection
             backgroundWorkerRead.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorkerRead_RunWorkerCompleted);
         }
 
-        public static class connectionClient {
-            public static S7Client S7Client;
-            public static NpgsqlConnection PGCon;
-        }
-
-        private void Button_Connect_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Установка соединения с PLC
-                connectionClient.S7Client = new S7Client();
-                int result = connectionClient.S7Client.ConnectTo("192.168.100.150", 0, 1);
-                if (result == 0)
-                {
-                    Console.WriteLine("Connected to 192.168.100.150");
-                }
-                else
-                {
-                    MessageBox.Show(connectionClient.S7Client.ErrorText(result));
-                }
-
-                // Установка соединения с PostgreSQL
-                string connectString = "Host=localhost;Username=postgre;Password=123456789;Database=_test_table";
-                connectionClient.PGCon = new NpgsqlConnection(connectString);
-                connectionClient.PGCon.Open();
-            }
-            catch (Exception ex) {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void Button_disConnect_Click(object sender, EventArgs e)
-        {
-            try {
-                // Закрытие соединений с PLC и PostgreSQL
-                connectionClient.S7Client.Disconnect();
-                connectionClient.PGCon.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            } 
-        }
-
         private void Button_Read_Click(object sender, EventArgs e)
         {
-            Stopwatch sw = Stopwatch.StartNew();
-            sw.Start();
             if (backgroundWorkerRead.IsBusy != true)
             {
                 // Start the asynchronous operation.
                 backgroundWorkerRead.RunWorkerAsync();
             }
-            sw.Stop();
-            Console.WriteLine("Array.Copy: {0:N0} ticks", sw.ElapsedTicks);
         }
 
         private void Button_notRead_Click(object sender, EventArgs e)
@@ -112,28 +59,66 @@ namespace Heineken_DataCollection
                 {
                     try
                     {
-                        List<string> myList = new List<string>();
-                        int result;
-                        for (int i = 0; i <= 99; i++)
+                        Stopwatch sw = Stopwatch.StartNew();
+                        sw.Start();
+                        // Установка соединения с PLC
+                        S7Client plcClient = new S7Client();
+                        //connectionClient.S7Client = new S7Client();
+                        int result = plcClient.ConnectTo("192.168.100.150", 0, 1);
+                        if (result == 0)
                         {
-                            byte[] db1Buffer = new byte[4];
-                            result = connectionClient.S7Client.DBRead(2000, 432 + 4 * i, 4, db1Buffer);
-                            if (result != 0)
-                            {
-                                Console.WriteLine("Error: " + connectionClient.S7Client.ErrorText(result));
-                            }
+                            //Console.WriteLine("Connected to 192.168.100.150");
+                        }
+                        else
+                        {
+                            MessageBox.Show(plcClient.ErrorText(result));
+                        }
+                        
+                        // Установка соединения с PostgreSQL
+                        NpgsqlConnection PGCon = new NpgsqlConnection("Host=localhost;Username=postgres;Password=123456789;Database=postgres");
+                        PGCon.Open();
+                        
+                        List<string> myList = new List<string>();
 
-                            double db1ddd4 = S7.GetRealAt(db1Buffer, 0);
-                            myList.Add("(" + i + "," + db1ddd4.ToString().Replace(",", ".") + ",'" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "')");
+                        byte[] db1Buffer = new byte[800];
+                        result = plcClient.DBRead(2000, 432, 800, db1Buffer);
+                        if (result != 0)
+                        {
+                            Console.WriteLine("Error: " + plcClient.ErrorText(result));
+                        }
+
+                        for (int i = 0; i <= 199; i++) {
+                            double db1ddd4 = S7.GetRealAt(db1Buffer, 4*i);
+                            myList.Add("(" + i + "," + db1ddd4.ToString().Replace(",", ".") + ",'" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "')");
                         }
                         var test = String.Join(", ", myList.ToArray());
 
                         // Запиись данных в PostgreSQL
-                        var cmd_insert = new NpgsqlCommand();
-
-                        cmd_insert.Connection = connectionClient.PGCon;
-                        cmd_insert.CommandText = "INSERT INTO _test_table (id, value, date_time) VALUES " + test;
+                        var cmd_insert = new NpgsqlCommand
+                        {
+                            Connection = PGCon,
+                            CommandText = "INSERT INTO _test_table (id, value, date_time) VALUES " + test
+                        };
                         cmd_insert.ExecuteNonQuery();
+
+                        progressBarRead.Invoke(new Action(() => progressBarRead.Style = ProgressBarStyle.Marquee));
+
+                        sw.Stop();
+                        Console.WriteLine("Read: {0:N0} ticks", sw.ElapsedTicks);
+
+                        // Закрытие соединений с PLC и PostgreSQL
+                        plcClient.Disconnect();
+                        PGCon.Close();
+                        // !!! For Grafana --- Do not DELETE !!!
+                        /*
+                        SELECT
+                        ((date_time) - INTERVAL '7 HOUR') AS "time",
+                        value
+                        FROM _test_table
+                        WHERE
+                        id = 1
+                        ORDER BY 1
+                        */
                     }
                     catch (Exception ex)
                     {
@@ -144,7 +129,8 @@ namespace Heineken_DataCollection
         }
         private void BackgroundWorkerRead_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-
+            progressBarRead.Invoke(new Action(() => progressBarRead.Value = 0));
+            progressBarRead.Invoke(new Action(() => progressBarRead.Style = ProgressBarStyle.Blocks));
         }
     }
 }
